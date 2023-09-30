e43c7952e8ba7147dfc47a1bda6bdb71b3c4f5ec
shivanknarula <shivanknarula@gmail.com>
2023-09-30 17:57:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+import requests
+import json
+import math
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Python program to print
+# colored text and background
+def strRed(skk):         return "\033[91m {}\033[00m".format(skk)
+def strGreen(skk):       return "\033[92m {}\033[00m".format(skk)
+def strYellow(skk):      return "\033[93m {}\033[00m".format(skk)
+def strLightPurple(skk): return "\033[94m {}\033[00m".format(skk)
+def strPurple(skk):      return "\033[95m {}\033[00m".format(skk)
+def strCyan(skk):        return "\033[96m {}\033[00m".format(skk)
+def strLightGray(skk):   return "\033[97m {}\033[00m".format(skk)
+def strBlack(skk):       return "\033[98m {}\033[00m".format(skk)
+def strBold(skk):        return "\033[1m {}\033[0m".format(skk)
+
+# Method to get nearest strikes
+def round_nearest(x, num=50): return int(math.ceil(float(x) / num) * num)
+def nearest_strike_bnf(x): return round_nearest(x, 100)
+def nearest_strike_nf(x): return round_nearest(x, 50)
+
+url_oc = "https://www.nseindia.com/option-chain"
+url_bnf = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
+url_nf = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+url_indices = "https://www.nseindia.com/api/allIndices"
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'accept-language': 'en,gu;q=0.9,hi;q=0.8',
+    'accept-encoding': 'gzip, deflate, br'}
+
+sess = requests.Session()
+cookies = dict()
+
+# Local methods
+def set_cookie():
+    request = sess.get(url_oc, headers=headers, timeout=5)
+    cookies = dict(request.cookies)
+
+def get_data(url):
+    set_cookie()
+    response = sess.get(url, headers=headers, timeout=5, cookies=cookies)
+    if (response.status_code == 401):
+        set_cookie()
+        response = sess.get(url_nf, headers=headers, timeout=5, cookies=cookies)
+    if (response.status_code == 200):
+        return response.text
+    return ""
+
+def set_header():
+    global bnf_ul
+    global nf_ul
+    global bnf_nearest
+    global nf_nearest
+
+    response_text = get_data(url_indices)
+    data = json.loads(response_text)
+    for index in data["data"]:
+        if index["index"] == "NIFTY 50":
+            nf_ul = index["last"]
+        if index["index"] == "NIFTY BANK":
+            bnf_ul = index["last"]
+
+    bnf_nearest = nearest_strike_bnf(bnf_ul)
+    nf_nearest = nearest_strike_nf(nf_ul)
+
+def print_header(index="", ul=0, nearest=0):
+    return (
+        strPurple(index.ljust(12, " ") + " => ") + strLightPurple(" Last Price: ") + strBold(str(ul)) + strLightPurple(
+            " Nearest Strike: ") + strBold(str(nearest)))
+
+def print_hr():
+    return strYellow("|".rjust(70, "-"))
+
+def print_oi(num, step, nearest, url):
+    strike = nearest - (step * num)
+    start_strike = nearest - (step * num)
+    response_text = get_data(url)
+    data = json.loads(response_text)
+    currExpiryDate = data["records"]["expiryDates"][0]
+    result = []
+    result.append(strPurple( "Expiry".ljust(12, " ") + " =>  ") + strLightPurple(currExpiryDate))
+    result.append(print_hr())
+    for item in data['records']['data']:
+        if item["expiryDate"] == currExpiryDate:
+            if item["strikePrice"] == strike and item["strikePrice"] < start_strike + (step * num * 2):
+                result.append(strCyan(str(item["strikePrice"])) + strGreen(" CE ") + "[ " + strBold(
+                    str(item["CE"]["openInterest"]).rjust(10, " ")) + " ]" + strRed(" PE ") + "[ " + strBold(
+                    str(item["PE"]["openInterest"]).rjust(10, " ")) + " ]")
+                strike = strike + step
+    return result
+
+@app.route("/")
+def option_chain():
+    set_header()
+    option_chain_data = {
+        "nifty_header": print_header("Nifty", nf_ul, nf_nearest),
+        "nifty_oi": print_oi(5, 50, nf_nearest, url_nf),
+        "banknifty_header": print_header("Bank Nifty", bnf_ul, bnf_nearest),
+        "banknifty_oi": print_oi(10, 100, bnf_nearest, url_bnf),
+    }
+    return render_template("option_chain.html", data=option_chain_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
